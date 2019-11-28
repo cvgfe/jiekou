089298301c3b4110aef27fc48006e7e50eaf2135
cvgfe <56747794+cvgfe@users.noreply.github.com>
2019-11-28 12:03:47 +0800

    Update myJs.js

--- asset/myJs.js
+++ asset/myJs.js
@@ -1,10 +1,10 @@
 window.onload = function(){
     let thetxt = document.getElementById('tXt');
-    let word = thetxt.value;
+    let keyword = thetxt.value;
     document.getElementById('btn').onclick = function(){
-        let request = new.XMLHttpRequest();
-        request.open("GET","/music/search/唐人/1/10")
-        request.send(word);
+        let request = new XMLHttpRequest();
+        request.open("GET","https://api.github.com/search/repositories?q=${keyword}+in:name&sort=stars&order=desc")
+        request.send();
         request.onreadystatechange = function(){
         if(request.readyState === 4){
             if(request.status === 200){
