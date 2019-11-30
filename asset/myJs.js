19f7c05cda66cc3746ba5c67290ce195b941c431
cvgfe <56747794+cvgfe@users.noreply.github.com>
2019-11-30 22:14:24 +0800

    Update myJs.js

--- asset/myJs.js
+++ asset/myJs.js
@@ -1,18 +1,28 @@
 window.onload = function(){
-    let thetxt = document.getElementById('tXt');
-    let keyword = thetxt.value;
-    document.getElementById('btn').onclick = function(){
-        let request = new XMLHttpRequest();
-        request.open("GET","https://api.github.com/search/repositories?q=${keyword}+in:name&sort=stars&order=desc");
-        request.send();
-        request.onreadystatechange = function(){
-        if(request.readyState === 4){
-            if(request.status === 200){
-                const json = JSON.parse(request.responseText);
-                console.log(json);
-                console.log(request.responseText);
-             }
-            }
+  let thetxt = document.getElementById('tXt');
+  let react = thetxt.value;
+  let Ul = document.querySelector('ul');
+  document.getElementById('btn').onclick = function(){
+      let request = new XMLHttpRequest();
+      request.open("GET","https://api.github.com/search/repositories?q=react+in:name&sort=stars&order=desc");
+      request.send(react);
+      request.onreadystatechange = function(){
+      if(request.readyState === 4){
+          if(request.status === 200){
+              let json = JSON.parse(request.responseText);
+            
+             console.log(json);
+            
+             
+            
+
+            let Li = document.createElement('li')
+             Ul.appendChild(Li);
+           Li.innerText = request.responseText
+            
+            
+          }
         }
-    }
-}
+          }
+      }
+  }
